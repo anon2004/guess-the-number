5ec96be4f7452d5d37876b69c5d30cda325411b0
Beamer Devs <83333147+anon2004@users.noreply.github.com>
2023-11-25 00:28:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 
+from random import randint
+number=randint(1,100)
+counter=0
+
+while True:
+  guess=int(input("Guess a number between 1 and 100: "))
+  counter=counter+1
+  if guess<0:
+    exit()
+
+  if guess>number:
+     print("Too high Try again")
+     continue
+  elif guess<number:
+    print("Too low Try again")
+    continue
+  else:
+   break
+print(f"You got it {number} after {counter} attempts!")
